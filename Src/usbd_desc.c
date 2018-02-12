b83eec57af900bba1d3f8e2034c9b2816895dc91
Evan Mortimore <evan.mortimore@crossmatch.com>
2018-02-12 15:09:27 -0800

    Add project files from STMCubeMX -> Eclipse

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -93,9 +93,9 @@
 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "Evan Mortimore Ltd"
+#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
 #define USBD_PID_FS     22315
-#define USBD_PRODUCT_STRING_FS     "Custom Low-power Input Thing - Mouse Mode"
+#define USBD_PRODUCT_STRING_FS     "STM32 Human interface"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "HID Config"
 #define USBD_INTERFACE_STRING_FS     "HID Interface"
